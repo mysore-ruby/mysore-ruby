11422e697f6f2cfc02cf471533b7e64273e3b993
Prakash Murthy <prakblr@yahoo.com>
2013-03-07 12:11:33 +0530

    Initial commit, after setting up gems & db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysoreRuby::Application.config.secret_token = 'cdff9abc2100477764895cb7f3fa22bc25a4be2a29f4b417b79f1311695ec5fa12c2f62213d9f33b372bacaf54c3da7439511b8ede57888fcf4dd85ab1549c3a'
